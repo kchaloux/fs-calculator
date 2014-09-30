69e2f4123aea3659afbdc339f2e07d94550068b4
KChaloux <kvnchaloux@gmail.com>
2014-09-30 15:11:27 -0400

    Add support for Scientific Notation, improve verbose debugging text, improve output formatting, refactor

--- calc.fs
+++ calc.fs
@@ -18,15 +18,26 @@ let getEvaluator = function
   | Add -> (+)
   | Subtract -> (-)
 
+let operatorSymbols =
+  [ "^", Exponent
+    "*", Multiply
+    "%", Modulus
+    "/", Divide
+    "+", Add
+    "-", Subtract ]
+
+let symbolToOperator = operatorSymbols |> Map.ofList
+let operatorToSymbol = operatorSymbols |> List.map (fun (x, y) -> (y, x)) |> Map.ofList
+
 let getOperator str =
-  match str with
-  | "^" -> Exponent
-  | "*" -> Multiply
-  | "%" -> Modulus
-  | "/" -> Divide
-  | "+" -> Add
-  | "-" -> Subtract
-  | other -> failwith <| "Expected operator, found '" + other + "'"
+  match symbolToOperator |> Map.tryFind str with
+  | Some op -> op
+  | _ -> failwith <| sprintf "Expected operator, found '%s'" str
+
+let getSymbol op =
+  match operatorToSymbol |> Map.tryFind op with
+  | Some symbol -> symbol
+  | _ -> failwith <| sprintf "Could not find a symbol for operator '%A'" op
 
 type ExpressionTree =
   | Expression of
@@ -41,7 +52,7 @@ let rec evalExpr = function
 
 let rec addExpr op right expr =
   match expr with
-  | Value left -> Expression (expr, op, right)
+  | Value _ -> Expression (expr, op, right)
   | Expression (left, op2, right2) ->
     if (getPrecedence op <= getPrecedence op2) then
       Expression(expr, op, right)
@@ -49,36 +60,50 @@ let rec addExpr op right expr =
       Expression(left, op2, addExpr op right right2)
 
 let op = @"[\/\+\-\*\^%]"
-let number = @"-?(\.\d+|\d+\.?\d*)"
-let tokenPattern = @"((?<!" + op + ")-" + "|" + number + "|" + op + ")"
+let decimal = @"-?(\.\d+|\d+\.?\d*)"
+let scientific = sprintf @"%s[eE]-?\d+" decimal
+let tokenPattern = sprintf @"((?<!%s)-|%s|%s|%s)" op scientific decimal op
 let tokenRegex = Regex(tokenPattern, RegexOptions.Compiled)
 
 let checkSyntax str =
   tokenRegex.Replace(str, "") |> Seq.filter ((<>)' ') |> Seq.isEmpty
 
+let tokenize str = seq { for x in tokenRegex.Matches(str) do yield x.Value }
+
 let parseExpr str =
-  let tokens = seq { for x in tokenRegex.Matches(str) do yield x.Value }
+  let tokens = tokenize str
   let initial = tokens |> Seq.head |> double |> Value
   tokens
   |> Seq.skip 1
   |> Seq.pairwise
   |> Seq.mapi (fun i x -> i % 2 = 0, x)
   |> Seq.filter fst
-  |> Seq.map (fun (i, (op, value)) -> getOperator op, value |> double |> Value)
+  |> Seq.map (fun (_, (op, value)) -> getOperator op, value |> double |> Value)
   |> Seq.fold (fun exprTree (op, value) -> addExpr op value exprTree) initial
+
+let showValue n =
+  (sprintf "%0.15f" n).TrimEnd('0').TrimEnd('.')
+
+let rec showExpr expr =
+  match expr with
+  | Value n -> showValue n
+  | Expression (left, op, right) ->
+    sprintf "(%s%s%s)" (showExpr left) (getSymbol op) (showExpr right)
 
 let (<||>) f g x = f x || g x
 
 [<EntryPoint>]
 let main args =
   try
-    if not <| checkSyntax args.[0] then
-      failwith <| "Could not parse '" + args.[0] + "'"
+    let str = args.[0]
+    if not <| checkSyntax str then
+      failwith <| sprintf "Could not parse '%s'" str
 
-    let expr = parseExpr args.[0]
+    let expr = parseExpr str
     if args |> Seq.exists ((=) "-v" <||> (=) "--verbose") then
-      printfn "%A" expr
-    printfn "%f" <| evalExpr expr
+      printfn "Tokens: %A" <| (tokenize str |> List.ofSeq)
+      printfn "Expression: %s" <| showExpr expr
+    printfn "%s" <| (expr |> evalExpr |> showValue)
     0
   with
     | ex ->
